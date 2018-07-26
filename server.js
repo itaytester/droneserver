0128f2940a61b8f3c3c01d044ad7d7828f1c736e
Itay Cohen <itaytester@gmail.com>
2018-07-26 12:03:20 +0300

    setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 1337;
+
+
+mongoose.connect('mongodb://localhost/mydb');                           // connect mongoose to db
+var db = mongoose.connection;                                           // get connection
+db.on('error', console.error.bind(console, 'connection error:'));       // on error listener
+db.once('open', () => {                                                 // on connection established
+  console.log('connected to MongoDB database: mydb');
+});
+
+app.set('maxDroneTail', 5)
+app.use(cors())                                                         // use the cors module
+app.use('/api/Drones', require('./api/drones.js'));                     // register the drone controller on /api/Drones/ route
+
+app.listen(port, () => {                                                // listen to port
+    console.log("listening in port " + port);
+});
