ae916c46d953f55f4339ad99ec76a6f4498eab85
yoni2356 <yoni2356@gmail.com>
2018-07-30 15:09:46 +0300

    feature updated

--- api/dronesController.js
+++ api/dronesController.js
@@ -1,45 +1,67 @@
 module.exports = (mongoose) => {
 
-const express = require('express');
-const bodyParser = require('body-parser')
-const router = express.Router();
-const Drone = require('../models/drone')(mongoose);
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const router = express.Router();
+    const Drone = require('../models/drone')(mongoose);
+    const Target = require('../models/target');
 
     router
-    .use(bodyParser.json())
-    .get('/',(req, res) => {
-        Drone.find({}, (err, drones) => {
-            if(err) res.status(500).send(err);
-            res.send(drones);
-        });
-    })
-    .post('/insert', (req, res) => {
-        var drone = req.body;
-        Drone.create(drone, (err, result) => {
-            if(err) res.status(500).send('Error upon insertation');
-            res.send('created');
-        });
-    })
-    .put('/Update',(req, res) => {
-        var droneId = req.body._id;
-        var data = req.body;
-        Drone.findById(droneId, (err, drone) => {
-            if(err) res.status(404).send('couldnt find drone');
-            drone.position = data.position;
-            drone.save((err, updatedDrone) => {
-                if(err) res.status(500).send('Error while updating drone');
-                res.send('drone was updated');
+        .use(bodyParser.json())
+        .get('/', (req, res) => {
+            Drone.find({}, (err, drones) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.send(drones);
+            });
+        })
+        .post('/insert', (req, res) => {
+            var drone = req.body;
+            Drone.create(drone, (err, result) => {
+                if (err) res.status(500).send('Error upon insertation');
+                res.send(result);
+            });
+        })
+        .post('/LinkTarget', (req, res) => {
+            var targetId = req.body.targetId;
+            var droneId = req.body.droneId;
+            Drone.findById(droneId, (err, drone) => {
+                if (err) res.status(404).send('drone not found');
+                drone.target_id = targetId;
+                drone.save((err, updatedDrone) => {
+                    if (err) res.status(500).send('Error while updating target of drone');
+                    res.send(targetId);
+                });
+            });
+        })
+        .post('/Update', (req, res) => {
+            var droneId = req.body._id;
+            var data = req.body;
+            Drone.findById(droneId, (err, drone) => {
+                if (err) res.status(404).send('could not find drone');
+                drone.position = data.position;
+                drone.save((err, updatedDrone) => {
+                    if (err) res.status(500).send('Error while updating drone');
+                    if (updatedDrone.target_id)
+                        Target.findById(updatedDrone.target_id, (err, target) => {
+                            if (err) res.send('could not find target in db');
+                            res.send(target.position);
+                        });
+                    else
+                        res.send('drone updated');
+                });
+            });
+        })
+        .delete('/Delete/:droneId', (req, res) => {
+            var droneId = req.params.droneId;
+            Drone.findByIdAndRemove(droneId, (err, result) => {
+                if (err) res.status(500).send("Error: drone was not removed");
+                res.send(result)
             });
         });
-    })
-    .delete('/Delete/:droneId',(req, res) => {
-        var droneId = req.params.droneId;
-        Drone.findByIdAndRemove(droneId, (err, result) => {
-            if(err) res.status(500).send("Error: drone wasnt removed");
-            res.send("remove success")
-        });
-    });
 
     return router;
 }
+
 //module.exports = router;
--- models/target.js
+++ models/target.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+module.exports = (mongoose) => {
+    const Schema = mongoose.Schema;
 
-var TargetSchema = new Schema({
-    type: String,
-    position: {lat: number, lng: number, alt: number},
-    source: String
-});
+    var TargetSchema = new Schema({
+        type: String,
+        position: {lat: Number, lng: Number, alt: Number},
+        source: String
+    });
 
-module.exports = mongoose.model('Target', TargetSchema, 'targets');
+    return mongoose.model('Target', TargetSchema, 'targets');
+}
